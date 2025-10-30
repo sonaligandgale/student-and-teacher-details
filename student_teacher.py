54b308d3c0204fe41dad8de59791ecf24873d062
sonaligandgale <sonaligandgalekar@gmail.com>
2025-10-30 15:04:23 +0530

    Create student_teacher.py

---
+++ student_teacher.py
@@ -0,0 +1,9 @@
+name = input("Enter the student name: ")
+age = int(input("Enter the student's age: "))
+usn = int(input("Enter the USN: "))
+address = input("Enter the address: ")
+
+print(f"Student name: {name}")
+print(f"Student age: {age}")
+print(f"Student USN: {usn}")
+print(f"Student address: {address}")
